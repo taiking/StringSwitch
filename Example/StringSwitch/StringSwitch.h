641d0aa8f306c44ad4bb786ab8df5fb5c6885c2d
chocovayashi <c.tsujiyan727@gmail.com>
2018-04-04 15:00:37 +0900

    make header

---
+++ Example/StringSwitch/StringSwitch.h
@@ -0,0 +1,19 @@
+//
+//  StringSwitch.h
+//  StringSwitch
+//
+//  Created by chocovayashi on 2018/04/04.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringSwitch.
+FOUNDATION_EXPORT double StringSwitchVersionNumber;
+
+//! Project version string for StringSwitch.
+FOUNDATION_EXPORT const unsigned char StringSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringSwitch/PublicHeader.h>
+
+
